4c75c42a35373c47db1af81164434a41fb5cf2a0
Claudio Vargas <cl.vargasparada@gmail.com>
2024-11-16 23:32:10 -0300

    toca subirlo a aws

--- src/index.py
+++ src/index.py
@@ -4,12 +4,9 @@ import datetime
 from dotenv import load_dotenv
 import os
 import urllib.request, json
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-import time
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+import requests
+
+import sqlite3
 
 
 #--------------Variables a ordenar--------------#
@@ -18,8 +15,8 @@ load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 
 chile_tz = datetime.datetime.now().astimezone().tzinfo
-hora_entrar = datetime.time(hour=18, minute=40, tzinfo=chile_tz)
-hora_salida = datetime.time(hour=18, minute=00, tzinfo=chile_tz)
+hora_entrar = datetime.time(hour=21, minute=21, tzinfo=chile_tz)
+hora_salida = datetime.time(hour=21, minute=26, tzinfo=chile_tz)
 
 bot = commands.Bot(command_prefix='>', description="Bot creado para MK", intents=discord.Intents.all())
 
@@ -37,96 +34,76 @@ async def info(ctx):
 
 @bot.command()
 async def registrar(ctx, *options):
+
+    dm_privado = await ctx.author.create_dm()
+
     channel = discord.utils.get(ctx.guild.text_channels, name='general')
     
-    
     if not options:
-       
         embed = discord.Embed(title="Registro", description="Proceso para el registro de asistencia.",
                           color=discord.Color.blue())
-        embed.add_field(name="Por favor.", value="utiliza '>registrar' seguido de tu nombre completo (nombre y apellidos)")
-        embed.add_field(name="Ejemplo:", value=">registrar PACIENTE PRUEBA GONZALES GONZALES")
+        embed.add_field(name="Por favor.", value="utiliza '>registrar' seguido de tu rut (con guion).")
+        embed.add_field(name="Ejemplo:", value=">registrar 11111111-1")
         embed.set_thumbnail(url="https://www.masterkey.cl/images/logo-mk-new.png")
         await channel.send(embed=embed)
         return
     
-    elif len(options) < 3: 
-            await channel.send("Por favor ingrese nombre completo")
-            return
+    if '-' not in options[0]: 
+        
+        await dm_privado.send("Por favor ingrese su rut con guion.")
+        return
+    
+    elif len(options[0]) < 10 or len(options[0]) > 10:
+        await dm_privado.send(f"Estimado, por favor verifique que su rut esté escrito correctamente: '{options[0]}'.")
+        await dm_privado.send(f"Le recuerdo que debe ser: '11111111-1'.")
+        return
     
     elif options:
+        rut = options[0]
         id_discord = ctx.author.id
 
-        nombre = ''    
-        for x in options:
-            nombre += x.capitalize()+ " "
-        nombre = nombre[:-1]
+        mi_conexion = sqlite3.connect("src/database/usuarios")
+        cursor = mi_conexion.cursor()
 
-        chrome_options = Options()
-        chrome_options.set_capability('goog:loggingPrefs', {'browser': 'ALL'})
-        
-        service = Service(executable_path=r"C:\dchrome\chromedriver.exe")
-        print("prosigo1")
-        driver = webdriver.Chrome(service=service, options=chrome_options)
-        driver.get("http://127.0.0.1:8000/registro/")
+        cursor.execute("SELECT * FROM usuario WHERE id_discord = ?", (id_discord,)) #Comprobamos si hay una usuario ya con el id de discord
+        resultado = cursor.fetchone()
 
-        button = driver.find_element(By.ID, "enviar")
-        input_id = driver.find_element(By.ID, "registro_id")
-        input_nombre = driver.find_element(By.ID, "registro_nombre")
-        input_sector = driver.find_element(By.ID, "registro_sector")
-        
-        input_id.send_keys(id_discord)
-        input_nombre.send_keys(nombre)
-        button.click()
-
-        time.sleep(0.01)
-
-        console_log = driver.get_log('browser')
-        console_log = console_log[0]['message']
-        
-        if "Ya se ha registrado" in console_log:
-            indice = console_log.index("Ya se ha registrado")
-            console_log = console_log[indice: -1]
-            dm_privado = await ctx.author.create_dm()
-            await dm_privado.send(f"Estima@ {nombre}. {console_log}.")
-            driver.quit()
+        if resultado:
+            await dm_privado.send(f"Estimado, usted ya se encuentra en nuestros registros.")
             return
-        
-        elif "Usuario registrado" in console_log:
-            indice = console_log.index("Usuario registrado")
-            console_log = console_log[indice: -1]
-            dm_privado = await ctx.author.create_dm()
-            await dm_privado.send(f"Estima@ {nombre}. {console_log}.")
-            driver.quit()
+        else:
+            cursor.execute("INSERT INTO usuario (id_discord, rut) VALUES (?, ?)", (id_discord, rut))
+            await dm_privado.send(f"Registro con exito! con el rut '{options[0]}'")
+            mi_conexion.commit()
             return
-
-        return
-
+    
 
     
-#evento del bot
+#--------------Eventos de bot--------------#
+
 @bot.event
 async def on_reaction_add(reaction: discord.Reaction, user):
     if user == bot.user:
         return
     
-    print(user.id)
-
     #
     if reaction.message.author == bot.user and str(reaction.emoji) == '✅':
-        dm_channel = await user.create_dm()
+        enviar_dm = await user.create_dm()
+        id_discord = user.id
 
-        discord_id = user.id
-        marcar_entrada(discord_id)
-        await dm_channel.send("Quedaste registrado.")
+        await enviar_dm.send(marcar_entrada(id_discord))
 
     if reaction.message.author == bot.user and str(reaction.emoji) == '❗':
-        return
+        enviar_dm = await user.create_dm()
+
+        id_discord = user.id
+        await enviar_dm.send(marcar_salida(id_discord))
     
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Streaming(name="bot entrenando", url="https://www.twitch.tv/rambodriving"))
     print('todo bien')
+    print(datetime.datetime.now())
     mensaje_entrada.start()
     mensaje_salida.start()
 
@@ -135,24 +112,28 @@ async def on_ready():
 async def on_member_join(member: discord.Member) -> None:
     channel = discord.utils.get(member.guild.text_channels, name='general')
     if channel:
-        await channel.send(f'Bienvenido ql {member.mention}')
-        await member.send('todo bien todo contento?')
+        await channel.send(f'Bienvenido {member.mention}')
+        await member.send('todo bien?')
 
 #--------------Hasta acá--------------
 
-#Mensaje de entrar
+
+#--------------Mensajes automatizados--------------#
+#Mensaje de salida
 @tasks.loop(time=hora_entrar)
 async def mensaje_entrada():
-    print("entré")
+    print("etnré al msj de entrada")
     hora = datetime.datetime.now(chile_tz).strftime("%H:%M")
     
-    if hora == hora_entrar.strftime("%H:%M") and not es_feriado():
-        guild = discord.utils.get(bot.guilds, id=827940399422111754)
+    if hora == hora_entrar.strftime("%H:%M") and not es_feriado(): #Agregar paraa sabado y domingo: and not datetime.datetime.today().weekday() == 5 and not datetime.datetime.today().weekday() == 6:
+
+        guild = discord.utils.get(bot.guilds, id=827940399422111754)  #Cambiar por id del sv que queremos enviar msj
 
         channel = discord.utils.get(guild.text_channels, name='general')
-        embed = discord.Embed(title="Marcar entrada", description="marquen entrada monos ql", 
+        embed = discord.Embed(title="Marcar entrada", description="Buenos días, recuerden marcar entrada!.", 
                           timestamp=datetime.datetime.utcnow() + datetime.timedelta(hours=-3), 
                           color=discord.Color.blue())
+        
         embed.set_thumbnail(url="https://www.masterkey.cl/images/logo-mk-new.png")
 
         msg = await channel.send(embed=embed)
@@ -162,22 +143,25 @@ async def mensaje_entrada():
 #Mensaje de salida
 @tasks.loop(time=hora_salida)
 async def mensaje_salida():
-    
+    print("entré mensaje salida")
     hora = datetime.datetime.now(chile_tz).strftime("%H:%M")
     
     if hora == hora_salida.strftime("%H:%M") and not es_feriado():
-        guild = discord.utils.get(bot.guilds, id=712797175838408775)
+
+        guild = discord.utils.get(bot.guilds, id=827940399422111754)
+        
         channel = discord.utils.get(guild.text_channels, name='general')
-        embed = discord.Embed(title="Marcar Salida", description="marquen salida monos ql", 
+        embed = discord.Embed(title="Marcar Salida", description="Hasta mañana, recuerden marcar salida!.", 
                           timestamp=datetime.datetime.utcnow() + datetime.timedelta(hours=-3), 
                           color=discord.Color.blue())
         embed.set_thumbnail(url="https://www.masterkey.cl/images/logo-mk-new.png")
 
         msg = await channel.send(embed=embed)
-        await msg.add_reaction('✅')
+        await msg.add_reaction('❗')
 
 
-#--------------Funciones--------------
+#--------------Funciones--------------#
+
 def es_feriado():
     url = "https://apis.digital.gob.cl/fl/feriados/2024"
     autori = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -194,19 +178,48 @@ def es_feriado():
         else:
             return False
 
-def marcar_entrada(discord_id):
+def marcar_entrada(id_discord):
 
-    service = Service(executable_path=r"C:\dchrome\chromedriver.exe")
-    driver = webdriver.Chrome(service=service)
+    mi_conexion = sqlite3.connect("src/database/usuarios")
+    cursor = mi_conexion.cursor()
 
-    driver.get("http://127.0.0.1:8000/registro/")
-    button = driver.find_element(By.ID, "entrada")
-    label = driver.find_element(By.ID, "registro_id_entrada")
+    cursor.execute("SELECT rut FROM usuario WHERE id_discord = ?", (id_discord,))
 
+    try:
+        rut = cursor.fetchone()[0]
+    except:
+        print("entreasds")
+        return (f"Usted no se encuentra en nuestra base de datos. Por favor use '>registro'")
+
+    request_entrada = requests.get('https://app.ctrlit.cl/ctrl/dial/registrarweb/EUqtz5lvAg?i=1&lat=&lng=&r=19275294-9') 
+    estado_entrada = request_entrada.content.decode('utf-8')
+
+    if estado_entrada == 'ok':
+        return (f"Ha marcado entrada con exito!.")
+    else:
+        return (f"Ya marcó entrada, usted no ha marcado salida.")
     
-    label.send_keys(discord_id)
-    button.click()
-    driver.quit()
-    return print("todo bien tod contento!")
+    
+def marcar_salida(id_discord):
+    mi_conexion = sqlite3.connect("src/database/usuarios")
+    cursor = mi_conexion.cursor()
+
+    cursor.execute("SELECT rut FROM usuario WHERE id_discord = ?", (id_discord,))
+
+    try:
+        rut = cursor.fetchone()[0]
+    except:
+        print("entre")
+        return (f"Usted no se encuentra en nuestra base de datos. Por favor use '>registro'")
+
+  
+    request_salida = requests.get('https://app.ctrlit.cl/ctrl/dial/registrarweb/EUqtz5lvAg?i=0&lat=&lng=&r=19275294-9') 
+    estado_salida = request_salida.content.decode('utf-8')
+
+    if estado_salida == 'ok':
+        return (f"Ha marcado salida con exito!.")
+    else:
+        return (f"Ya marcó salida, usted no ha marcado entrada.")
+
 
 bot.run(token)
